18e114986d30297b69c328bfa0a77a43bfe0bd90
Xuytaparsan <105146418+Xuytaparsan@users.noreply.github.com>
2022-05-20 10:12:27 +0400

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-	token: "5240844969:AAFctpnzMBtTQNCbRyv1fkY3OKvH552kUeg",
-	botId: 5240844969, //first numbers of BOT_TOKEN
+	token: "5392936718:AAGVfbbzdvz6i4tLDGqbfwvKK3gWMTfRPkM",
+	botId: 5392936718, //first numbers of BOT_TOKEN
 	rounds: 20, //rounds amount
 	waitDelay: 12000, //1 round duration
 	timerSteps: 4, //timer indicators
